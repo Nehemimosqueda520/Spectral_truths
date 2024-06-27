54c5d39d78e2cf8bd2a513ddf669ad63aa58c53b
lucasduran3 <lucasduran510@gmail.com>
2024-06-27 00:21:20 -0300

    rotacion suave y delay de linterna

--- Spectral truths/Assets/scripts/CameraScript.cs
+++ Spectral truths/Assets/scripts/CameraScript.cs
@@ -5,31 +5,36 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    [SerializeField] private float MouseSensitivity = 80f;
+    private float MouseSensitivity = 200f;
     [SerializeField] private Transform Player;
     [SerializeField] private Transform Lantern;
+    private float smoothTime = 0.2f;
+    private float lanternDelay = 12f;
 
     private float rotationX = 0f;
-    private float rotationY = 0f;
+    private float currentMouseX;
+    private float currentMouseY;
+    private float mouseXVelocity = 0f;
+    private float mouseYVelocity = 0f;
+    private Quaternion targetLanternRotation;
 
     void Update()
     {
-        float MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
-        float MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+        float targetMouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float targetMouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
 
-        rotationX -= MouseY;
+        currentMouseX = Mathf.SmoothDamp(currentMouseX, targetMouseX, ref mouseXVelocity, smoothTime);
+        currentMouseY = Mathf.SmoothDamp(currentMouseY, targetMouseY, ref mouseYVelocity, smoothTime);
+
+        rotationX -= currentMouseY;
         rotationX = Mathf.Clamp(rotationX, -90f, 90f);
 
-        rotationY += MouseX;
-
-
         transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
-        Player.Rotate(Vector3.up * MouseX);
+        Player.Rotate(Vector3.up * currentMouseX);
 
-        // Lantern.localRotation = Quaternion.Euler(0f, 0f, MouseX);
-        Quaternion targetRotation = Quaternion.Euler(rotationX, rotationY, 0f);
-        Lantern.rotation = targetRotation;
-        //Lantern.Rotate(Vector3.left * MouseY)
+        // Actualiza la rotación de la linterna con un retraso
+        targetLanternRotation = Quaternion.Lerp(targetLanternRotation, transform.rotation, lanternDelay * Time.deltaTime);
+        Lantern.rotation = targetLanternRotation;
     }
        
 }
